c78f7e40f5c7a120ef8df87d76b490896da7fe07
Omarfaruk05 <mdomarfrauk149518.com>
2022-04-18 23:08:28 +0600

    edit readmi.md file

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -2,14 +2,27 @@ import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetail = () => {
     const {serviceId} = useParams();
+    console.log(serviceId)
+    let choice;
+    if(serviceId === '1'){
+         choice = <h1 className='text-center'>You choose Basic card for hire</h1>;
+    }
+    if(serviceId === '2'){
+         choice = <h1>You choose Standard card for hire</h1>;
+    }
+    if(serviceId === '3'){
+         choice = <h1>You choose Premium card for hire</h1>;
+    }
     return (
-        <div style={{height:'80vh'}}>
-            <h3>Service Details</h3>
-            <Link to={'/checkout'}>
-               <div className='text-center mt-5'>
-                <button className='btn btn-primary'>Proceed Checkout</button>
-               </div>
-            </Link>
+        <div className='d-flex justify-content-center align-items-center' style={{height:'81vh'}} >
+           <div>
+                {choice}
+                <Link to={'/checkout'}>
+                <div className='text-center mt-5'>
+                    <button style={{backgroundColor:'#4b886f', fontWeight:'400'}} className='btn btn-primary'>Proceed Checkout</button>
+                </div>
+                </Link>
+           </div>
         </div>
     );
 };
--- src/Pages/Shared/Notfound/NotFound.js
+++ src/Pages/Shared/Notfound/NotFound.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const NotFound = () => {
     return (
-        <div className='d-flex align-items-center justify-content-center mt-5 pt-5 text-center'>
-            <div className='mt-5 pt-5'>
+        <div style={{height:'82vh'}} className='d-flex align-items-center justify-content-center  text-center'>
+            <div>
             <h1 style={{fontSize:'100px', color:'gray'}}>404</h1>
-            <h5 style={{marginBottom:'290px'}}>Page Not Found</h5>
+            <h5>Page Not Found</h5>
             </div>
         </div>
     );
