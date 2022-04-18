cf436bd7bc74e14f618afa21aab4998aeb3e30fc
Omarfaruk05 <mdomarfrauk149518.com>
2022-04-18 20:42:08 +0600

    bug fixing

--- src/Pages/ServiceDetail/ServiceDetail.js
+++ src/Pages/ServiceDetail/ServiceDetail.js
@@ -1,14 +1,10 @@
-import { useContext } from 'react';
 import { Link, useParams } from 'react-router-dom';
-import { ServicesContext } from '../Home/PricingTables/PricingTables';
 
 const ServiceDetail = () => {
     const {serviceId} = useParams();
-    const services = useContext(ServicesContext);
-    console.log(services)
     return (
         <div style={{height:'80vh'}}>
-            <h3>Service Details : {services}</h3>
+            <h3>Service Details</h3>
             <Link to={'/checkout'}>
                <div className='text-center mt-5'>
                 <button className='btn btn-primary'>Proceed Checkout</button>
